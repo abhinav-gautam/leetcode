d42d8ef9b79cab2a2967a1cc35b80a07348cdbe0
abhinav-gautam <abhinav22gautam@gmail.com>
2025-11-01 21:57:24 +0530

    add problems

---
+++ SQL/Basic Aggregate Functions/1633. Percentage of Users Attended a Contest.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/percentage-of-users-attended-a-contest/description/?envType=study-plan-v2&envId=top-sql-50
+
+select contest_id, round(count(*)*100/(select count(*) from users),2) as percentage 
+from register 
+group by contest_id 
+order by percentage desc ,contest_id asc;

876aa7ebcd0d522d564112951cead8453d6dc3d4
abhinav-gautam <abhinav22gautam@gmail.com>
2025-11-07 19:24:52 +0530

    add problems

---
+++ SQL/Sorting and Grouping/1141. User Activity for the Past 30 Days I.sql
@@ -0,0 +1,11 @@
+-- https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/?envType=study-plan-v2&envId=top-sql-50
+select
+    activity_date as day,
+    count(distinct user_id) as active_users
+from
+    activity
+where
+    activity_date between '2019-06-28'
+    and '2019-07-27'
+group by
+    activity_date;
---
+++ SQL/Sorting and Grouping/2356. Number of Unique Subjects Taught by Each Teacher.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/?envType=study-plan-v2&envId=top-sql-50
+select
+    teacher_id,
+    count(distinct subject_id) as cnt
+from
+    teacher
+group by
+    teacher_id;
